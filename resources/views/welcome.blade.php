2d031b9a9078a47013f61fc6fae0cdb80c1672d2
nshadybekov <nshadybekov@adsterra.com>
2022-01-09 21:50:12 +0300

    webpack obfucator

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Olivyerchik</title>
+        <title>CAFE 360</title>
 
         @include('styles')
     </head>
---
+++ tests/Unit/ContactTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ContactTest extends TestCase
+{
+
+    public function test_contact_form()
+    {
+        $this->assertTrue(true);
+    }
+}
